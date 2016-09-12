b2d4c3dc99815e73e1e749ed05b460c8bfce5194
Kyle Simek <kyle@matterport.com>
2016-09-12 13:07:06 -0700

    handling corner cases in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,17 +3,21 @@ pushd `dirname "$0"` > /dev/null
 SCRIPTPATH=$(pwd)
 popd > /dev/null
 
-set -eux
+set -eu
 
 function try_link()
 {
     file_=$1
     dst_=$2
+    if [[ -L $dst_ && "$(readlink $dst_) == $dst_)" ]]; then
+        echo "symlink for $file_ already exists"
+        return 0
+    fi
     if [[ -e $dst_ || -L $dst_ ]]; then
         yn=''
         while [[ "$yn" != 'y' && "$yn" != 'n' ]]; do
             echo "File $dst_ already exists.  Overwrite?"
-            read -N 1 -p '> ' yn < /dev/tty
+            read -n 1 -p '> ' yn < /dev/tty
             echo ''
         done
         if [[ "$yn" == 'y' ]]; then
@@ -27,11 +31,24 @@ function try_link()
 
 cat to_link.txt | while read file; do
     dst=~/$file
-    if [[ -d $SCRIPTPATH/$file ]]; then
+    src=$SCRIPTPATH/$file
+    if [[ -L "$dst" && ! "$dst" -ef "$src" ]]; then
+        echo "Error - different symlink exists at $dst.  Resolve and re-run setup.sh"
+        exit 1
+    fi
+
+    if [[ -d $src ]]; then
+        if [[ -L "$dst" && "$dst" -ef "$src" ]]; then
+            # this happens if you already symlinked these directories to $HOME, which creates problems
+            echo "Error: symlink link already exists and points to this path"
+            echo "src: $src"
+            echo "dst: $dst"
+            exit 1
+        fi
         mkdir -p $dst
         (
         shopt -s dotglob
-        for file2 in $SCRIPTPATH/$file/*; do
+        for file2 in $src/*; do
             dst2=~/$file/$(basename $file2)
             try_link $file2 $dst2
         done
